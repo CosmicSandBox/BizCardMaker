5b31c50c1cc902f8e508f6ba5e7c738195ee751a
kye1115z <kye1115z@naver.com>
2023-02-08 00:57:48 +0900

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html}"],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+//최적화 부분은 잘 모르겠네요..
