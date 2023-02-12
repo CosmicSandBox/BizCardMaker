f72a4049ca5063d6a3322a001ca43c82515c9a41
yeram-lim <yeramyeye@gmail.com>
2023-02-12 18:45:08 +0900

    😺[CHORE] home page

--- src/Component/Three.js
+++ src/Component/Three.js
@@ -1,25 +1,15 @@
 import { useRef, useState } from "react";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { OrbitControls, useTexture } from "@react-three/drei";
+import { OrbitControls, useTexture, Html } from "@react-three/drei";
 import React, { Suspense } from "react";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
 
 const name = (type) => `PavingStones092_1K_${type}.jpg`;
 
-function Box(props) {
-  // const [colorMap, displacementMap, normalMap, roughnessMap, aoMap] = useLoader(
-  //   TextureLoader,
-  //   [
-  //     name("Color"),
-  //     name("Displacement"),
-  //     name("Normal"),
-  //     name("Roughness"),
-  //     name("AmbientOcclusion"),
-  //   ]
-  // );
+function Box({ position, imgSrc }) {
   const [colorMap, displacementMap, normalMap, roughnessMap, aoMap] = useLoader(
     TextureLoader,
-    ["logo512.png", "logo512.png", "logo512.png", "logo512.png", "logo512.png"]
+    [imgSrc, imgSrc, imgSrc, imgSrc, imgSrc]
   );
   // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef();
@@ -27,40 +17,75 @@ function Box(props) {
   const [hovered, hover] = useState(false);
   const [clicked, click] = useState(false);
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += delta));
+  // useFrame((state, delta) => (ref.current.rotation.x += delta));
+
+  const [size, set] = useState(0.5);
+  const [hidden, setVisible] = useState(false);
+
   // Return the view, these are regular Threejs elements expressed in JSX
   return (
     <mesh
-      {...props}
+      // {...props}
+      position={position}
       ref={ref}
       scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
+      // onClick={(event) => click(!clicked)}
+      // onPointerOver={(event) => hover(true)}
+      // onPointerOut={(event) => hover(false)}
     >
-      <boxGeometry args={[4, 2, 0.03]} />
+      //TODO 1번쨰
+      {/* <boxGeometry args={[2, 2, 2]} /> {[4, 2, 0.03]}
+     
       <meshStandardMaterial
         displacementScale={0.2}
         // map={colorMap}
         // displacementMap={displacementMap}
-        normalMap={normalMap}
-        // roughnessMap={roughnessMap}
-        // aoMap={aoMap}
-        // color={hovered ? "hotpink" : "orange"}
-      />
+        // normalMap={normalMap} //얘가 앞면 같고?
+        // // roughnessMap={roughnessMap}
+        // aoMap={aoMap} //얘가 뒷면 같고
+        color={"white"}
+      /> */}
+      <boxGeometry args={[4, 2, 0.03]} />
+      <meshStandardMaterial normalMap={normalMap} />
+      <Html
+        style={{
+          transition: "all 0.2s",
+          opacity: hidden ? 0 : 1,
+          transform: `scale(${hidden ? 0.5 : 1})`,
+        }}
+        distanceFactor={0.5}
+        position={[0, 0, 0.03]}
+        transform
+        occlude
+        // onOcclude={setVisible}
+      >
+        <span
+          style={{
+            fontSize: "150px",
+            background: "pink",
+            width: "100%",
+            display: "flex",
+          }}
+        >
+          Size hi
+        </span>
+      </Html>
     </mesh>
   );
 }
 
-export default function App() {
+export default function App({ imgSrc }) {
   return (
-    <Canvas className={`canvas`} style={{ height: "50vh" }}>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box position={[0, 0, 0]} />
-      {/* <Box position={[1.2, 0, 0]} /> */}
-      <OrbitControls />
-    </Canvas>
+    <>
+      <Canvas className={`canvas`} style={{ height: "50vh" }}>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 10, 5]} />
+        {/* <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} /> */}
+        {/* <pointLight position={[-10, -10, -10]} /> */}
+        <Box imgSrc={imgSrc} />
+        {/* <Box position={[1.2, 0, 0]} /> */}
+        <OrbitControls />
+      </Canvas>
+    </>
   );
 }
