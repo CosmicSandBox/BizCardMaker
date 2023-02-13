5bcbfb488d74acf2a54ba1f410492f2f8eb6ce0f
yeram-lim <yeramyeye@gmail.com>
2023-02-14 00:14:56 +0900

    🍀[FEAT] select template component

--- src/router.js
+++ src/router.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from './Component/Home';
-import Info from './Component/Info';
-import Name from './Component/Name';
-import Three from "./Component/Three";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Component/Home";
+import Info from "./Component/Info";
+import Name from "./Component/Name";
 import Slide from "./Component/Slide";
 import HomePage from "./Component/HomePage";
+import SelectTamplate from "./Component/SelectTamplate";
 
 function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/info" element={<Info />} />
-                <Route path="/name" element={<Name />} />
-        <Route
-          path="/slide"
-          element={<Slide photos={[<h3>1</h3>, <h3>2</h3>, <h3>3</h3>]} />}
-        />
-        <Route path="/home" element={<HomePage />} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/info" element={<Info />} />
+        <Route path="/name" element={<Name />} />
+        {/* <Route path="/home" element={<HomePage />} /> */}
+        <Route path="/select-template" element={<SelectTamplate />} />
       </Routes>
     </BrowserRouter>
   );
