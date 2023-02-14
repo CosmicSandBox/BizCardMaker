656c709d14420fed14b08fd1e724802a1be96996
kye1115z <kye1115z@naver.com>
2023-02-14 21:28:09 +0900

    docs: App.js +GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import Router from "./router.js";
 import "./index.css";
 import Undermsg from "./Undermsg";
+import GlobalStyle from "./Component/GlobalStyle.js";
 
 function App() {
   return (
-    <div
-      style={{
-        maxWidth: "31.25rem",
-        width: "100vw",
-        height: "100vh",
-        margin: "0 auto",
-      }}
-    >
-      <Router />
-      <Undermsg />
-    </div>
+    <>
+      <GlobalStyle />
+      <div
+        style={{
+          maxWidth: "31.25rem",
+          width: "100vw",
+          height: "100vh",
+          margin: "0 auto",
+        }}
+      >
+        <Router />
+        <Undermsg />
+      </div>
+    </>
   );
 }
 
