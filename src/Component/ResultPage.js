3b7c50643f5486b894d673e745610e6df613435f
kye1115z <kye1115z@naver.com>
2023-02-18 05:38:41 +0900

    Add: share+result

--- src/Component/ResultPage.js
+++ src/Component/ResultPage.js
@@ -3,6 +3,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import Three from "./Three";
 import { Btn } from "../styles/basicStyles";
 import styled from "styled-components";
+import Share from "./Share"
 
 const ResultPageStyle = styled.section`
   & {
@@ -10,7 +11,7 @@ const ResultPageStyle = styled.section`
     flex-direction: column;
     align-items: center;
     flex: 1;
-    gap: 2rem;
+    gap: 1rem;
     padding: 2rem 0;
 
     > div:first-child,
@@ -24,6 +25,9 @@ const ResultPageStyle = styled.section`
     > div.comment {
       gap: 0.5rem;
       font-size: 1.375rem;
+      text-align: center;
+      white-space: pre-wrap;
+      font-family: 'yg-jalnan';
     }
   }
 `;
@@ -46,22 +50,28 @@ const ResultPage = () => {
     <>
       <ResultPageStyle>
         <div>
-          <div>{state.userInfo.name}님, 당신의 명함은</div>
+          <div style={{fontFamily: "yg-jalnan",
+          }}>{state.userInfo.name}님, 당신의 명함은</div>
           <Three
             frontImgSrc={state.frontImgUrl}
             backImgSrc={state.backImgUrl}
           />
         </div>
 
-        <Btn type="button" onClick={() => downloadImg()} color={"#FF7A00"}>
+        <Btn type="button" onClick={() => downloadImg()} color={"#FF7A00"}
+              style={{width: "50%", marginBottom: "3rem"}}>
           명함 다운받기
         </Btn>
 
-        <div className="comment">
-          <span>다른 외대생들도 명함을</span>
-          <span>만들 수 있게 도와줘!</span>
-          <span>지금 바로 공유하기</span>
+        <div className="comment"
+              style={{fontSize: "1rem"}}>
+                다른 외대생들도 명함을<br/>만들 수 있게 도와줘!<br/>
         </div>
+        <div className="comment" 
+              style={{whiteSpace: "pre-wrap"}}>
+                지금 바로 공유하기
+        </div>
+        <Share />
 
         <Btn type="button" onClick={null} color={"#FF7A00"}>
           다른 명함 만들러 가기
