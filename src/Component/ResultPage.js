c33168318fb9cfaa0bf55d20e9e4f62b0c65f1d5
yeram-lim <yeramyeye@gmail.com>
2023-02-23 19:39:30 +0900

    😺[CHORE] separate biz card download (#Issue_Number)

--- src/Component/ResultPage.js
+++ src/Component/ResultPage.js
@@ -14,23 +14,29 @@ const ResultPageStyle = styled.section`
     flex: 1;
     gap: 1rem;
     padding: 44px 0;
-
   }
 `;
 
 const ResultPage = () => {
   const navigate = useNavigate();
   const { state } = useLocation();
-  const downloadImg = () => {
+
+  const downloadImg = (side) => {
     const links = [state.frontImgUrl, state.backImgUrl];
-    for (let i = 0; i < links.length; i++) {
-      const link = document.createElement("a");
-      document.body.appendChild(link);
-      link.href = links[i];
-      link.download = `like-lion-business-card-${i}.png`;
-      link.click();
-      document.body.removeChild(link);
+
+    const link = document.createElement("a");
+    document.body.appendChild(link);
+
+    if (side === "front") {
+      link.href = links[0];
+      link.download = `like-lion-business-card-${0}.png`;
+    } else {
+      link.href = links[1];
+      link.download = `like-lion-business-card-${1}.png`;
     }
+
+    link.click();
+    document.body.removeChild(link);
   };
 
   return (
@@ -41,119 +47,160 @@ const ResultPage = () => {
             backgroundColor: "#FAF7F4",
             paddingTop: "20px",
             textAlign: "center",
-            fontFamily: 'ONE-Mobile-POP',
+            fontFamily: "ONE-Mobile-POP",
             borderRadius: "20px",
-            boxShadow: "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
+            boxShadow:
+              "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
             width: "80%",
             display: "flex",
             flexDirection: "column",
             justifyContent: "center",
             alignItems: "center",
-            marginBottom: "5%"
-          }}>
-        <div>
-          <div style={{fontFamily: 'ONE-Mobile-POP', fontSize: "1.4rem", marginBottom: "1rem", paddingBottom: "5%", borderBottom: "solid 1px lightgray" }}>
-            {state.userInfo.name}님, 당신의 명함은
-          </div>
-          <Three
-            frontImgSrc={state.frontImgUrl}
-            backImgSrc={state.backImgUrl}
+            marginBottom: "5%",
+          }}
+        >
+          <div>
+            <div
+              style={{
+                fontFamily: "ONE-Mobile-POP",
+                fontSize: "1.4rem",
+                marginBottom: "1rem",
+                paddingBottom: "5%",
+                borderBottom: "solid 1px lightgray",
+              }}
+            >
+              {state.userInfo.name}님, 당신의 명함은
+            </div>
+            <Three
+              frontImgSrc={state.frontImgUrl}
+              backImgSrc={state.backImgUrl}
             />
-        </div>
+          </div>
 
-        <Btn
-          type="button"
-          onClick={() => downloadImg()}
-          color={"#FF7A00"}
-          style={{ width: "45%", marginBottom: "2rem", marginTop: "1.3rem" }}
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              width: "100%",
+              gap: "1rem",
+            }}
           >
-          명함 다운받기
-        </Btn>
+            <Btn
+              type="button"
+              onClick={() => downloadImg("front")}
+              color={"#FF7A00"}
+              style={{
+                width: "40%",
+                marginBottom: "2rem",
+                marginTop: "1.3rem",
+              }}
+            >
+              명함 앞면<br></br> 다운받기
+            </Btn>
+            <Btn
+              type="button"
+              onClick={() => downloadImg("back")}
+              color={"#FF7A00"}
+              style={{
+                width: "40%",
+                marginBottom: "2rem",
+                marginTop: "1.3rem",
+              }}
+            >
+              명함 뒷면<br></br> 다운받기
+            </Btn>
+          </div>
         </div>
-        <div 
+        <div
           style={{
             backgroundColor: "#FAF7F4",
             padding: "20px",
             textAlign: "center",
-            fontFamily: 'ONE-Mobile-POP',
+            fontFamily: "ONE-Mobile-POP",
             borderRadius: "20px",
-            boxShadow: "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
+            boxShadow:
+              "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
             width: "80%",
             display: "flex",
             flexDirection: "column",
             justifyContent: "center",
             alignItems: "center",
-            marginBottom: "5%"
-            
-          }}>
+            marginBottom: "5%",
+          }}
+        >
           <div
-            style={{ fontSize: "1.3rem", background: "transparent", lineHeight: "130%",
-            marginBottom: "3%"}}
-            >
+            style={{
+              fontSize: "1.3rem",
+              background: "transparent",
+              lineHeight: "130%",
+              marginBottom: "3%",
+            }}
+          >
             다른 외대생들도 명함을
             <br />
             만들 수 있게 도와줘!
             <br />
           </div>
-          
-          <div 
+
+          <div
             style={{
-              width: "90%", 
-              backgroundColor: "lightgray", 
+              width: "90%",
+              backgroundColor: "lightgray",
               height: "1px",
-              marginBottom: "4%"}}>
+              marginBottom: "4%",
+            }}
+          ></div>
 
-          </div>
-
-        <Share />
+          <Share />
         </div>
-          
-          <div 
-            style={{
-              width: "90%",  
-              height: "1px",
-              display: "flex",
-              justifyContent: "center",
-              marginBottom: "15%"}}>
+
+        <div
+          style={{
+            width: "90%",
+            height: "1px",
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: "15%",
+          }}
+        >
           <Btn
-              type="button"
-              onClick={() => {
-                navigate("/");
-              }}
-              color={"#FF7A00"}
-              style={{
-                      borderRadius: "20px",
-                      width: "90%"}}
-              >
-              다른 명함 만들러 가기
-          </Btn>
-
-        </div>
-
-        <div 
+            type="button"
+            onClick={() => {
+              navigate("/");
+            }}
+            color={"#FF7A00"}
             style={{
-              width: "90%",  
-              height: "1px",
-              display: "flex",
-              justifyContent: "center",
-              marginBottom: "10%"}}>
-          <Btn
-              type="button"
-              onClick={() => {
-                window.open("https://www.instagram.com/hufsglobal_likelion/");
-              }}
-              color={"#FF7A00"}
-              style={{
-                      borderRadius: "20px",
-                      width: "90%"}}
-              >
-              새로운 명함 디자인 추천하러 가기
+              borderRadius: "20px",
+              width: "90%",
+            }}
+          >
+            다른 명함 만들러 가기
           </Btn>
-
         </div>
 
-
-
+        <div
+          style={{
+            width: "90%",
+            height: "1px",
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: "10%",
+          }}
+        >
+          <Btn
+            type="button"
+            onClick={() => {
+              window.open("https://www.instagram.com/hufsglobal_likelion/");
+            }}
+            color={"#FF7A00"}
+            style={{
+              borderRadius: "20px",
+              width: "90%",
+            }}
+          >
+            새로운 명함 디자인 추천하러 가기
+          </Btn>
+        </div>
       </ResultPageStyle>
     </>
   );
